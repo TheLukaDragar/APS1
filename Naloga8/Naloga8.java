31843dfecb9e7acc1def4276445da431e19c10fe
TheLukaDragar <lukadragarbusiness@gmail.com>
2022-01-06 21:45:15 +0100

    n8 dela

---
+++ Naloga8/Naloga8.java
@@ -0,0 +1,187 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.print.attribute.standard.PrinterStateReason;
+
+public class Naloga8 {
+
+    public static void main(String[] args) throws IOException {
+
+        String line;
+        int n;
+        BufferedReader br;
+
+        br = new BufferedReader(new FileReader(args[0]));
+        n = Integer.parseInt(br.readLine());
+
+        HashMap<Integer, Node> nodes = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+
+            line = br.readLine();
+
+            nodes.put(Integer.parseInt(line.split(",")[0]), new Node(Integer.parseInt(line.split(",")[0]),
+                    Integer.parseInt(line.split(",")[1]),
+                    Integer.parseInt(line.split(",")[2]),
+                    Integer.parseInt(line.split(",")[3])));
+
+        }
+
+        br.close();
+
+        Node rootNode = findRootNode(nodes);
+
+        BinaryTree tree = new BinaryTree(rootNode, nodes);
+        Writer out = new FileWriter(args[1]);
+
+        out.write(tree.printTree());
+        out.close();
+
+    }
+
+    static Node findRootNode(HashMap<Integer, Node> nodes) {
+
+        Set<Integer> p_id = new TreeSet<Integer>();
+        Set<Integer> all_id = new TreeSet<Integer>();
+
+        for (Node node : nodes.values()) {
+            if (node.id_l != -1) {
+                p_id.add(node.id_l);
+            }
+            if (node.id_r != -1) {
+                p_id.add(node.id_r);
+            }
+            all_id.add(node.id);
+
+        }
+        all_id.removeAll(p_id);
+        return nodes.get(all_id.iterator().next());
+
+    }
+
+    static class BinaryTree {
+        Node root;
+
+        HashMap<Integer, Node> nodes;
+
+        public BinaryTree(Node rootNode, HashMap<Integer, Node> nodes) {
+
+            this.root = rootNode;
+            this.nodes = nodes;
+
+            this.root.y = 0;
+
+            // recursevly build tree from nodes
+            buildTree(root);
+
+            getX(new ArrayList<Node>(), root, 0);
+
+            
+
+        }
+
+        private void buildTree(Node root) {
+
+            if (root.id_l != -1) {
+                root.left = nodes.get(root.id_l);
+                root.left.parent = root;
+                root.left.y = root.y + 1;
+                root.left.x = root.x - 1;
+                buildTree(root.left);
+
+            }
+            if (root.id_r != -1) {
+                root.right = nodes.get(root.id_r);
+                root.right.parent = root;
+                root.right.y = root.y + 1;
+                root.right.x = root.x + 1;
+                buildTree(root.right);
+
+            }
+
+        }
+
+    
+
+        void getX(ArrayList<Node> elements, Node node, int x) {
+
+            if (node == null) {
+                return;
+            }
+
+            if (x == -1)
+                x++;
+            elements.add(x, node);
+            getX(elements, node.left, x);
+
+            x = -1;
+            for (int i = 0; i < elements.size(); i++) {
+                if (elements.get(i).v == node.v) {
+                    x = i;
+                }
+            }
+
+            getX(elements, node.right, x + 1);
+            node.x = x;
+        }
+
+        public String printTree() {
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+            StringBuilder sb = new StringBuilder();
+
+            while (!queue.isEmpty()) {
+                Node node = queue.poll();
+                sb.append(node.v).append(",").append(node.x).append(",").append(node.y)
+                        .append("\n");
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            return sb.toString();
+
+        }
+
+    }
+
+    static class Node {
+
+        int id, v, id_l, id_r;
+        Node parent;
+        int x;
+        int y;
+        Node left;
+        Node right;
+
+        public Node(int id, int v, int id_l, int id_r) {
+            this.id = id;
+            this.v = v;
+            this.id_l = id_l;
+            this.id_r = id_r;
+        }
+
+        public int getId_l() {
+            return id_l;
+        }
+
+        public int getId_r() {
+            return id_r;
+        }
+    }
+
+}
